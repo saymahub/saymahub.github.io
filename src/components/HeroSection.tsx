847b3e51a8ae7d9b0243216622cdac98863a22ff
Sayma Haque <sayma.haque@ucalgary.ca>
2025-06-30 15:29:35 -0600

    saturn gif import

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,3 +1,5 @@
+import saturngif from '../assets/ringed.gif';
+
 export const HeroSection = () => {
     return (
         <section id="hero" className="relative min-h-screen flex flex-col items-center justify-center bottom-45 px-4">
@@ -6,7 +8,7 @@ export const HeroSection = () => {
                     Sayma Haque
                 </h1>
                 <div className="animate-float">
-                    <img src="../../public/ringed.gif" alt="saturn gif" className="opacity-0 animate-fade-in absolute left-1/2 -translate-y-[60px] md:top-0 md:-translate-y-[190px]
+                    <img src={saturngif} alt="saturn gif" className="opacity-0 animate-fade-in absolute left-1/2 -translate-y-[60px] md:top-0 md:-translate-y-[190px]
                      -translate-x-1/2 rotate-30 w-3xl h-3xl z-20 pointer-events-none"></img>
                 </div>
             </div>
