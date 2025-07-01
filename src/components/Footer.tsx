86f64a09b2eee0a25665790c7868d1a0c33ede47
Sayma Haque <sayma.haque@ucalgary.ca>
2025-06-30 22:13:58 -0600

    mobile vers fix with navbar and menu

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -23,7 +23,7 @@ export const Footer = ({ isMenuOpen, setIsMenuOpen }: FooterProps) => {
     }, []);
 
     return (
-        <nav className={cn("hidden md:flex fixed w-full bg-[color:var(--background)] bottom-0 left-1/2 transform -translate-x-1/2 z-40 transition-all duration-300", 
+        <nav className={cn("fixed w-full md:bg-[color:var(--background)] bg-[color:var(--background)]/0 bottom-0 left-1/2 transform -translate-x-1/2 z-40 transition-all duration-300", 
             isScrolled ? "py-3 bg-[color:var(--background)] backdrop-blur-mid" : "py-5"
         )}>
             <div className="container mx-auto flex items-center py-4 justify-center">
@@ -38,7 +38,7 @@ export const Footer = ({ isMenuOpen, setIsMenuOpen }: FooterProps) => {
                 </div>
 
                 {/* mobile nav */}
-                <div className={cn("fixed inset-0 bg-background/95 backdrop-blur-md z-40 flex flex-col items-center justify-center transition-all duration-30 md:hidden",
+                <div className={cn("fixed inset-0 opacity-0 z-40 flex flex-col items-center justify-center transition-all duration-30 md:hidden",
                     isMenuOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none")}>
                     <div className="flex flex-col space-y-8 -translate-y-20">
                         {footItems.map((item, key) => (
