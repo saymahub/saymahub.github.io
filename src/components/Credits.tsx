02375462918e122d90d9ef1bd806e79c82847ec6
Sayma <sayma.haque@ucalgary.ca>
2026-05-02 16:22:42 -0600

    easter egg for khalil

--- src/components/Credits.tsx
+++ src/components/Credits.tsx
@@ -8,11 +8,26 @@ export const Credits = () => {
         setTimeout(() => setRevealed(false), 3000);
     };
 
+    const [revealed2, setRevealed2] = useState(false);
+
+    const revealSecret2 = () => {
+        setRevealed2(true);
+        setTimeout(() => setRevealed2(false), 3000);
+    };
+
     return (
         <section id="credits" className="md:snap-start md:h-screen relative min-h-screen flex items-center justify-center bottom-45 z-10 pt-0 md:pt-10 translate-y-[20px]">
             <div className="relative container max-w-4xl mx-auto text-center z-10 ">
                 <h2 className="text-3xl md:text-5xl text-glow mb-12">
-                    This Site Wouldn't Be Possible Without <span className="text-[30px]"> ⋆⋆⋆</span>
+                    This Site Wouldn't Be Possible Without <span className="text-[30px]"> </span>
+                    <button onClick={revealSecret2}> 
+                            <span className="text-[10px] md:text-[30px]">⋆⋆⋆</span>
+                    </button>
+                    {revealed2 && (
+                        <span className="absolute left-1/2 transform -translate-x-1/2 translate-y-[30px] md:translate-y-[50px] mt-1 text-[20px] text-primary bg-[color:var(--primary)]/10 px-2 py-1 rounded">
+                            Khalil Taylor I've sent a bomb to ur residence
+                        </span>
+                    )}
                 </h2>
                 <p className="font-prata text-[18px] md:text-[24px] pb-7 space-y-5 md:px-30">
                     <a href="https://deep-fold.itch.io/pixel-planet-generator" target="_blank" rel="noopener noreferrer" className="underline inline-block hover:text-[color:var(--hovertext)] duration-100 hover:scale-110">
